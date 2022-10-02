bb9c03f30c606b91c01e945eeb2b49700aceb777
Bereket Terefe <berelliant11@gmail.com>
2022-10-03 00:56:18 +0300

    [fix] - projet view fixed... minor improvements

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,14 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import coffee_1 from "../assets/img/coffee_land_1.png";
-import coffee_2 from "../assets/img/coffee_land.png";
+// import coffee_2 from "../assets/img/coffee_land.png";
 import lego_1 from "../assets/img/lego_1.png";
-import lego_2 from "../assets/img/lego_2.png";
-import lego_3 from "../assets/img/lego_3.png";
+// import lego_2 from "../assets/img/lego_2.png";
+// import lego_3 from "../assets/img/lego_3.png";
 // import bitcoin_1 from "../assets/img/bitcoin_1.png"
-import bitcoin_2 from "../assets/img/bitcoin_2.png"
+// import bitcoin_2 from "../assets/img/bitcoin_2.png"
 // import colorSharp2 from "../assets/img/color-sharp2.png";
+import typography from "../assets/img/typography_1.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 import { useContext } from "react";
@@ -20,31 +21,34 @@ export const Projects = () => {
       title: "CoffeeLessonz Website",
       description: "A landing website for a promising EdTech startup.",
       imgUrl: coffee_1,
+      uri: 'https://coffeelessonz.com'
     },
-    {
-      title: "CoffeeLessonz Website",
-      description: "A landing website for a promising EdTech startup.",
-      imgUrl: coffee_2,
-    },
+    // {
+    //   title: "CoffeeLessonz Website",
+    //   description: "A landing website for a promising EdTech startup.",
+    //   imgUrl: coffee_2,
+    // },
     {
       title: "Lego Website",
-      description: "A Lego themed website built for fun.",
+      description: "An open source, lego themed, website built for fun.",
       imgUrl: lego_1,
+      uri: "https://github.com/rav3n11/lego-website"
     },
+    // {
+    //   title: "Lego Website",
+    //   description: "A Lego themed website built for fun.",
+    //   imgUrl: lego_2,
+    // },
+    // {
+    //   title: "Lego Website",
+    //   description: "A Lego themed website built for fun.",
+    //   imgUrl: lego_3,
+    // },
     {
-      title: "Lego Website",
-      description: "A Lego themed website built for fun.",
-      imgUrl: lego_2,
-    },
-    {
-      title: "Lego Website",
-      description: "A Lego themed website built for fun.",
-      imgUrl: lego_3,
-    },
-    {
-      title: "Bitcoin.et",
-      description: "A website for a cryptocurrency exchange.",
-      imgUrl: bitcoin_2,
+      title: "Typography Website",
+      description: "An open source website to showcase the power of typography.",
+      imgUrl: typography,
+      uri: "https://github.com/rav3n11/typography"
     }
   ];
 
